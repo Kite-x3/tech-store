33248151d915058bc660f27e1a9744a71dc571c0
SystemX <igorevich.05@bk.ru>
2025-03-22 18:32:08 +0300

    added Identity

--- TechStore.Domain/Entities/User.cs
+++ TechStore.Domain/Entities/User.cs
@@ -1,16 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace TechStore.Domain.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
-        [Key]
-        public int UserId { get; set; }
-
-        public string Username { get; set; }
-        public string Email { get; set; }
-
-        public string Password { get; set; }
     }
 }
--- TechStore.Infrastracture/Data/AppDbContext.cs
+++ TechStore.Infrastracture/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TechStore.Domain.Entities;
 
 namespace TechStore.Infrastracture.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<User>
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
